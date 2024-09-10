6db8b8cd40c89427abcf55d4f671714c84cdd30d
md5502 <m.baniasadi.d@gmail.com>
2024-09-10 19:43:53 +0330

    Implement user registration functionality
    - Add a registration view using RegisterForm.
    - Handle POST requests for user registration with form validation.
    - Display success message and redirect to login page after registration.
    - Provide error feedback for invalid form submissions.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful! You can now log in.')
+            return redirect('/accounts/login/')  # Redirect to the login page after successful registration
+        else:
+            messages.error(request, 'Please correct the errors below.', )
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'users/register.html', {'form': form})
