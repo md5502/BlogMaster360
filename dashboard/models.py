cd74c833426bfb1aaefcd8a7cc9d4c62fbb23cc6
md5502 <m.baniasadi.d@gmail.com>
2024-09-10 19:28:12 +0330

    create the profile model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField("Profile image", default='default_profile.png', null=True, blank=True, upload_to="uploads/users/profiles")
+    publish_date = models.DateField(auto_now_add=True)
+    
+    created_at = models.DateTimeField(verbose_name="Created At", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated At", auto_now=True)
+
+    
+    def __str__(self):
+        return self.user.username
