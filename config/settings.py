f54520322b384a139cd20a1b4dd5c8c6e3b81c53
md5502 <m.baniasadi.d@gmail.com>
2024-09-10 19:26:58 +0330

    add the crispy_forms and the configuration for it

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
